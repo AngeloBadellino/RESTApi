09c4e235391c5debf932833986fbcd6946ed26a0
Angelo Badellino <Badellinoa@gmail.com>
2022-05-24 11:47:22 +0200

    Fixed typos

--- index.js
+++ index.js
@@ -6,15 +6,15 @@
 var http = require('http');
 var url = require('url');
 
-// The server should respond to all the incoming request with a string.
+// The server should respond to all the incoming requests with a string.
 var server = http.createServer(function(req, res){
 
-    // Get and parse the url
+    // Get and parse the request's url
     var parsedURL = url.parse(req.url, true);
     var path = parsedURL.pathname.replace('/^\/+|\/+$/g', '');
 
     // Send the response
-    console.log('The server is listining on port 3000 now.')
+    console.log('The server is listening on port 3000 now.')
 
     // Logging the request
     console.log('Request received on ' + path);
